65fee10cd9d4d5d823830e0e2fdd8a8a705c1f51
janWICKEDpro <janroyal45@gmail.com>
2022-05-23 17:52:00 +0100

    first commit

---
+++ src/application/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.sql.*;
+public class DatabaseConnection {
+	Connection link;
+	
+	Connection getConnection() {
+		String databaseName = "njangi";
+		String databaseUser = "root";
+		String databasePassword = "janwickedpro";
+		String url = "jdbc:mysql://localhost:33060/" + databaseName;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			link = DriverManager.getConnection(url, databaseUser, databasePassword);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return link;
+	}
+}
